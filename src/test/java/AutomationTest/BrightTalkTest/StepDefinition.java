7a1e2296e6b2a8eb259fa4d6e1fd8f3ca9c62005
Rajno1 <rajasekhar.rct@gmail.com>
2020-07-13 14:28:00 +0530

    Test task

---
+++ src/test/java/AutomationTest/BrightTalkTest/StepDefinition.java
@@ -0,0 +1,13 @@
+package AutomationTest.BrightTalkTest;
+
+import cucumber.api.java.en.Given;
+
+import static AutomationTest.BrightTalkTest.HomePage.homePage;
+
+public class StepDefinition {
+
+    @Given("^I am on the home page$")
+    public void iAmOnTheHomePage() {
+        homePage();
+    }
+}
